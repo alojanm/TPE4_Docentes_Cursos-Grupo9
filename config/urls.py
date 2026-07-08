0abbb56dd04a2a18c7b7c237030ffa74a7494708
Alojanm <alojanm2@unemi.edu.ec>
2026-07-08 00:17:51 -0500

    Configuración PostgreSQL y modelo Docente

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 def inicio(request):
     return render(request, 'index.html')
 
@@ -26,3 +30,7 @@ urlpatterns = [
     path('', inicio, name='inicio'),
 ]
 
+if settings.DEBUG:
+
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    
